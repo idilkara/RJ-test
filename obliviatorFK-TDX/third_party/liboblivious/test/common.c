9f118f575ab7f4877fb5800319ca7aba64c75fcf
ahmednafis00 <nafis.ahmed@uwaterloo.ca>
2025-08-03 05:24:26 +0000

    baselines

--- obliviatorFK-TDX/third_party/liboblivious/test/common.c
+++
@@ -1,6 +0,0 @@
-#include "common.h"
-#include <stdlib.h>
-
-unsigned long get_random(void) {
-    return ((unsigned long) rand() << 32) | rand();
-}
--- obliviatorFK-TDX/third_party/liboblivious/test/oram.h
+++
@@ -1,6 +0,0 @@
-#ifndef LIBOBLIVIOUS_TEST_ORAM_H
-#define LIBOBLIVIOUS_TEST_ORAM_H
-
-char *test_oram(void);
-
-#endif /* liboblivious/test/oram.h */
